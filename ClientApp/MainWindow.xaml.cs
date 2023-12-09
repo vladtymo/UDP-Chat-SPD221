0dcf0b2b3da93e8f8a6260782e10c9310b31f3d9
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-12-09 15:29:23 +0200

    Add project files.

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        UdpClient client = new UdpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string ip = ipTB.Text;
+            int port = int.Parse(portTB.Text);
+
+            string message = msgTB.Text;
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            client.Send(Encoding.UTF8.GetBytes(message), endpoint);
+
+            // TODO: run receiving in Task
+
+            IPEndPoint? server = null;
+            var data = client.Receive(ref server);
+
+            var response = Encoding.UTF8.GetString(data);
+            dialogList.Items.Add(response);
+        }
+    }
+}
---
+++ ServerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    internal class Program
+    {
+        const int port = 3300;
+        static void Main(string[] args)
+        {
+            UdpClient server = new UdpClient(port);
+
+            IPEndPoint clientIP = null;
+
+            while (true)
+            {
+                // wait new request from client
+                Console.WriteLine("\t...Waiting new requests...");
+                var data = server.Receive(ref clientIP);
+
+                var message = Encoding.UTF8.GetString(data);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Request: {message} at {DateTime.Now} from {clientIP}");
+                Console.ResetColor();
+
+                // send response to the client
+                server.Send(Encoding.UTF8.GetBytes("Good!"), clientIP);
+            }
+        }
+    }
+}
